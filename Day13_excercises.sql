9b8f3d9a3e80440625b144a86854ddf9db51b47e
anhtho7301 <109756153+anhtho7301@users.noreply.github.com>
2024-02-25 21:22:48 -0600

    Update Day13_excercises.sql

--- Day13_excercises.sql
+++ Day13_excercises.sql
@@ -73,30 +73,21 @@ FROM a
 WHERE month <> 7
 
 --ex6:
-WITH a AS 
-(SELECT
-EXTRACT(year_month from trans_date) AS month,
+SELECT
+CONCAT(LEFT(EXTRACT(year_month from trans_date),4),'-',RIGHT(EXTRACT(year_month from trans_date),2)) AS month,
 country,
 COUNT(state) AS trans_count,
-SUM(amount) AS trans_total_amount
+SUM(amount) AS trans_total_amount,
+SUM(CASE
+    WHEN state='approved' THEN 1
+    ELSE 0
+END) AS approved_count,
+SUM(CASE
+    WHEN state='approved' THEN amount
+    ELSE 0
+END) AS approved_total_amount
 FROM Transactions
-GROUP BY month, country),
-b AS
-(SELECT
-EXTRACT(year_month from trans_date) AS month,
-country,
-COUNT(state) AS approved_count,
-SUM(amount) AS approved_total_amount
-FROM Transactions
-WHERE state='approved'
-GROUP BY month, country)
-SELECT 
-CONCAT(LEFT(a.month,4),'-',RIGHT(a.month,2)) AS month, a.country,
-a.trans_count, b.approved_count, 
-a.trans_total_amount, b.approved_total_amount
-FROM a
-JOIN b
-ON a.month=b.month AND a.country=b.country
+GROUP BY month, country
 
 --ex7:
 SELECT DISTINCT product_id,
@@ -104,8 +95,8 @@ year AS first_year,
 quantity,
 price
 FROM Sales
-WHERE year in
-(SELECT MIN(year) 
+WHERE (product_id, year) IN
+(SELECT product_id, MIN(year)
 FROM Sales 
 GROUP BY product_id)
 
@@ -113,7 +104,7 @@ GROUP BY product_id)
 SELECT customer_id
 FROM Customer
 GROUP BY customer_id
-HAVING COUNT(product_key)=2
+HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(DISTINCT product_key) FROM Product)
 
 --ex9:
 SELECT employee_id
@@ -121,44 +112,43 @@ FROM Employees
 WHERE NOT manager_id IN 
 (SELECT employee_id FROM Employees)
 AND salary<30000
+ORDER BY employee_id
 
 --ex10: same ex1
   
 --ex11:
 WITH a AS
-(SELECT r.user_id,
-u.name,
-COUNT(r.rating) AS count
+(SELECT
+u.name AS results
 FROM MovieRating AS r
 JOIN Users AS u
 ON r.user_id=u.user_id
-GROUP BY user_id
-ORDER BY count DESC, name
+GROUP BY u.name
+ORDER BY COUNT(r.rating) DESC, name
 LIMIT 1),
 b AS
 (SELECT
-r.movie_id,
-m.title,
-AVG(r.rating) AS avg_rating
+m.title AS results
 FROM MovieRating AS r
 JOIN Movies AS m
 ON r.movie_id=m.movie_id
-WHERE EXTRACT(month from created_at)=2
-GROUP BY movie_id
-ORDER BY avg_rating DESC, title
+WHERE EXTRACT(year_month from created_at)=202002
+GROUP BY m.title
+ORDER BY AVG(r.rating) DESC, title
 LIMIT 1)
-SELECT name AS results FROM a
-UNION 
-SELECT title FROM b
+SELECT results FROM a
+UNION ALL
+SELECT results FROM b
 
 --ex12:
 WITH a AS
-(SELECT requester_id, accepter_id FROM RequestAccepted
-UNION
-SELECT accepter_id, requester_id FROM RequestAccepted)
-SELECT requester_id AS id,
-COUNT(accepter_id) AS num
+(SELECT requester_id AS id
+    FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id AS id
+    FROM RequestAccepted)
+SELECT id, COUNT(id) AS num
 FROM a
 GROUP BY id
-ORDER BY num DESC
+ORDER BY COUNT(id) DESC
 LIMIT 1
